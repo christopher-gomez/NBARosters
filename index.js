fcaba3f2dab98903097d03e28e641c6b8030e89a
christopher_gomez <christopher_gomez@wayforward.com>
2023-03-30 22:14:10 -0700

    Google App Service Setup + Cloud Build

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const app = require("./server");
+
+// Determine mongoDB URL
+let murl = "mongodb://localhost:27017/NBA";
+if (process.env.NODE_ENV === "production") {
+  murl = process.env.MONGODB_URL;
+}
+
+let server;
+// Connect to mongo
+mongoose.connect(murl);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongoose connection error"));
+db.once("open", () => {
+  console.log("mongoose connection succeeded");
+
+  // Determine server port
+  const port = process.env.PORT || 3000;
+
+  // Listen for connections on port
+  server = app.listen(port, () => {
+    logger.info(`Listening to port ${port}`);
+  });
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received");
+  if (server) {
+    server.close();
+  }
+});
--- server.js
+++ server.js
@@ -1,53 +1,42 @@
 // Dependiencies/middleware
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-const serveStatic = require('serve-static');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const serveStatic = require("serve-static");
+const path = require("path");
 
 // Create the app (Express instance, app entrypoint)
 const app = express();
-const server = require('http').createServer(app);
 
 // Set up middleware
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false,
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cors());
 
-// Determine mongoDB URL
-let murl = 'mongodb://localhost:27017/NBA';
-if (process.env.NODE_ENV === 'production') {
-  murl = process.env.PROD_MONGODB;
-}
-
-// Connect to mongo
-mongoose.connect(murl);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'mongoose connection error'));
-db.once('open', () => {
-  console.log('mongoose connection succeeded');
-});
-
 // Set up express API routes
-const nbaRoutes = require('./express_routes/nba_api.js');
+const nbaRoutes = require("./express_routes/nba_api.js");
 
-app.use('/nba', nbaRoutes);
+app.use("/nba", nbaRoutes);
 
 // If not accessing the API, serve up the frontend
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 // Catch all routes and redirect to the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
+const server = require("http").createServer(app);
 
-// Determine server port
-const port = process.env.PORT || 3000;
+module.exports = server;
 
-// Listen for connections on port
-server.listen(port);
+// // Determine server port
+// const port = process.env.PORT || 3000;
+
+// // Listen for connections on port
+// server.listen(port);
