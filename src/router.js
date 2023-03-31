e05c090a52f8583ce9cadab6c2799a42d9babfbf
christopher_gomez <christopher_gomez@wayforward.com>
2023-03-30 23:39:06 -0700

    Added App URL env config for router history mode

--- src/router.js
+++ src/router.js
@@ -5,8 +5,12 @@ import PlayerView from './views/Players.vue';
 
 Vue.use(Router);
 
+console.log('APP URL: ');
+console.log(process.env.APP_URL);
+
 export default new Router({
   mode: 'history',
+  base: process.env.APP_URL,
   routes: [
     {
       path: '/',
